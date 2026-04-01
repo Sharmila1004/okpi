9d59488b6032cd83d4e39c0a330576ee3a03689d
Sharmila1004 <Sharmila.Sasikumar@laerdal.com>
2026-04-01 15:40:29 +0530

    Initial commit

---
+++ src/main/java/com/laerdal/okpi/OkpiApplication.java
@@ -0,0 +1,11 @@
+package com.laerdal.okpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkpiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OkpiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/laerdal/okpi/OkpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerdal.okpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
